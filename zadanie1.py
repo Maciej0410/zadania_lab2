878f39bce12700f008a590aa9d78d184ce5ec7da
Maciej0410 <76994645+Maciej0410@users.noreply.github.com>
2021-03-12 14:04:32 +0100

    Add files via upload

---
+++ zadanie1.py
@@ -0,0 +1,18 @@
+lista=['koszykowka', 'pilka nozna', 'pilka reczna', 'skoki narciarskie', 'tenis', 'hokej', 'boks']
+
+print(lista)
+
+lista.reverse()
+
+print(lista)
+
+lista.remove("boks")
+lista.append("boks")
+
+lista.remove("hokej")
+lista.append("hokej")
+
+lista.remove("tenis")
+lista.append("tenis")
+
+print(lista)
---
+++ zadanie2.py
@@ -0,0 +1,5 @@
+slownik_skrotow={'wyd.': 'wydanie', 'red.': 'redakcja', 'wydawn.': 'wydawnictwo', 'praca zbior.': 'praca zbiorowa' }
+
+print(slownik_skrotow['praca zbior.'])
+
+
